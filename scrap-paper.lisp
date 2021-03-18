13f2fef3ed55565ae3cf7b810d8461a4777c8713
prescottbreeden <prescott.breeden@gmail.com>
2021-03-18 00:37:37 -0700

    db

--- scrap-paper.lisp
+++ scrap-paper.lisp
@@ -1,10 +1,8 @@
-;;; Scrap Paper
-; (defun fizz-buzz(num)
-;   (cond ((and (= 0 (mod num 3)) (= 0 (mod num 5))) "fizz-buzz")
-;         ((= 0 (mod num 3)) "fizz")
-;         ((= 0 (mod num 5)) "buzz")
-;         (t (write-to-string num))))
+; Scrap Paper
+(defparameter *test-list* '(1 (a b c) 2 (d e f) 3))
+(car *test-list*) ; => 1
+(cdr *test-list*) ; => ((A B C) 2 (D E F) 3)
+(cadr *test-list*) ; => (A B C)
+(car (cadr *test-list*)) ; => A
 
-; (defun play-fizz()
-;   (loop for n from 1 to 100
-;         collect (fizz-buzz n)))
+
